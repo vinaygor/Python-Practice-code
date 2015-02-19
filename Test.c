d88bbefb64a6a94826f1cf3f463a0b942ab31216
vinaygor <vinaygor11@gmail.com>
2015-02-19 19:17:01 +0200

    First FIle

---
+++ Test.c
@@ -0,0 +1,25 @@
+
+#include<stdio.h>
+main()
+{
+    int i=0;
+    int j=0;
+    i=i++;
+    j++;
+    printf("i=%d\nj=%d\n",i,j);
+    i=i++;
+    j++;
+    printf("i=%d\nj=%d",i,j);
+    i=i++;
+    j++;
+    printf("i=%d\nj=%d\n",i,j);
+    i=i++;
+    j++;
+    printf("i=%d\nj=%d\n",i,j);
+    i=i++;
+    j++;
+    printf("i=%d\nj=%d\n",i,j);
+    i=i++;
+    j++;
+    printf("i=%d\nj=%d\n",i,j);
+}
